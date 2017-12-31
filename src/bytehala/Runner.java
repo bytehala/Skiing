7428544c1046d64247eb517539db89f1ee75c760
Lem Dulfo <lemuel.dulfo@gmail.com>
2017-12-31 23:13:01 +0800

    Solution with sample problem in Runner

--- src/bytehala/Runner.java
+++ src/bytehala/Runner.java
@@ -8,7 +8,15 @@ package bytehala;
 public class Runner {
 	
 	public static void main(String[] args) {
-		
+		int[][] field = {
+				{4, 8, 7, 3},
+				{2, 5, 9, 3},
+				{6, 3, 2, 5},
+				{4, 4, 1, 6}
+		};
+		Skiing s = new Skiing(4, 4, field);
+		s.solve();
+		System.out.println(s.getLongestRide() + " " + s.getHighestDrop());
 	}
 
 }
--- src/bytehala/Skiing.java
+++ src/bytehala/Skiing.java
@@ -16,7 +16,62 @@ public class Skiing {
 	}
 	
 	public void solve() {
+		for(int y = 0; y < h; y++) {
+			for(int x = 0; x < w; x++) {
+				if(isPeak(x, y)) {
+					skiFrom(x, y, 1, 0);
+				}
+			}
+		}
+	}
+	
+	private void skiFrom(int x, int y, int currentRideLength, int currentDrop) {
+		if (canGoNorth(x, y)) {
+			skiFrom(x, y-1, currentRideLength + 1, currentDrop + getAltitude(x, y) - getAltitude(x, y-1));
+		}
+		if (canGoSouth(x, y)) {
+			skiFrom(x, y+1, currentRideLength + 1, currentDrop + getAltitude(x, y) - getAltitude(x, y+1));
+		}
+		if (canGoEast(x, y)) {
+			skiFrom(x+1, y, currentRideLength + 1, currentDrop + getAltitude(x, y) - getAltitude(x+1, y));
+		}
+		if (canGoWest(x, y)) {
+			skiFrom(x-1, y, currentRideLength + 1, currentDrop + getAltitude(x, y) - getAltitude(x-1, y));
+		}
 		
+		if(currentRideLength >= longestRide && currentDrop > highestDrop) {
+			longestRide = currentRideLength;
+			highestDrop = currentDrop;
+		}
+	}
+	
+	private int getAltitude(int x, int y) {
+		return field[y][x];
+	}
+
+	boolean isPeak(int x, int y) {
+		return 	(y == 0 || getAltitude(x, y) > getAltitude(x, y-1)) &&// north
+				(y == h - 1 || getAltitude(x, y) > getAltitude(x, y+1)) && //south
+				(x == w - 1 || getAltitude(x, y) > getAltitude(x+1, y)) && // east
+				(x == 0 || getAltitude(x, y) > getAltitude(x-1, y));// west;
+	}
+	
+	private boolean canGoNorth(int x, int y) {
+		return y > 0 && getAltitude(x, y) > getAltitude(x, y-1);
+	}
+	
+	boolean canGoSouth(int x, int y) {
+		return y < h-1 && getAltitude(x, y) > getAltitude(x, y+1);
+		
+	}
+	
+	boolean canGoEast(int x, int y) {
+		return x < w-1 && getAltitude(x, y) > getAltitude(x+1, y);
+		
+	}
+	
+	boolean canGoWest(int x, int y) {
+		return x > 0 && getAltitude(x, y)  > getAltitude(x-1, y);
 	}
 	
 	public int getLongestRide() {
